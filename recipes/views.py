736a7b506c70401ae4a3b9be5ad308322ba3bc25
Ricardo Lima Fauzel <ricardofauzel@gmail.com>
2023-07-20 23:14:56 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Ricardo Fauzel'
     })
-
-
-def contato(request):
-    return render(request, 'me-apague/temp.html')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
